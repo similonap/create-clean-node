3b9e9b45c47df01aab5acef6e52d741b315d056c
Andie Similon <andie.similon@ap.be>
2024-03-11 21:24:43 +0000

    1.0.6

--- template_express_ejs/index.ts
+++ template_express_ejs/index.ts
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 
 app.set("port", process.env.PORT || 3000);
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
     res.render("index", {
         title: "Hello World",
         message: "Hello World"
